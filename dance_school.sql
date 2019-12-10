2de4b5a1b6128864e47add4b95c23256a352e680
DrDersu <58595037+DrDersu@users.noreply.github.com>
2019-12-10 11:34:16 +0600

    Add files via upload

--- dance_school.sql
+++ dance_school.sql
@@ -1,22 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+-- version 3.3.9
+-- http://www.phpmyadmin.net
 --
--- Хост: 127.0.0.1
--- Время создания: Дек 09 2019 г., 11:48
--- Версия сервера: 10.1.35-MariaDB
--- Версия PHP: 7.2.9
+-- Хост: localhost
+-- Время создания: Дек 10 2019 г., 06:33
+-- Версия сервера: 5.5.8
+-- Версия PHP: 5.3.5
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- База данных: `dance_school`
@@ -28,12 +25,19 @@ SET time_zone = "+00:00";
 -- Структура таблицы `admin`
 --
 
-CREATE TABLE `admin` (
+CREATE TABLE IF NOT EXISTS `admin` (
   `login` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
   `name` varchar(500) NOT NULL,
   `income` int(250) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `admin`
+--
+
+INSERT INTO `admin` (`login`, `password`, `name`, `income`) VALUES
+('admin', 'admin', 'Erlan', 0);
 
 -- --------------------------------------------------------
 
@@ -41,8 +45,8 @@ CREATE TABLE `admin` (
 -- Структура таблицы `adult`
 --
 
-CREATE TABLE `adult` (
-  `id` int(250) NOT NULL,
+CREATE TABLE IF NOT EXISTS `adult` (
+  `id` int(250) NOT NULL AUTO_INCREMENT,
   `title` varchar(500) CHARACTER SET utf8 NOT NULL,
   `master` varchar(500) CHARACTER SET utf8 NOT NULL,
   `schedule` varchar(500) CHARACTER SET utf8 NOT NULL,
@@ -50,8 +54,14 @@ CREATE TABLE `adult` (
   `sold` bigint(255) DEFAULT '0',
   `style` varchar(500) CHARACTER SET utf8 NOT NULL,
   `gender` varchar(500) CHARACTER SET utf8 NOT NULL,
-  `indiv_group` varchar(500) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `indiv_group` varchar(500) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
+
+--
+-- Дамп данных таблицы `adult`
+--
+
 
 -- --------------------------------------------------------
 
@@ -59,14 +69,20 @@ CREATE TABLE `adult` (
 -- Структура таблицы `clients`
 --
 
-CREATE TABLE `clients` (
-  `id` int(250) NOT NULL,
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(250) NOT NULL AUTO_INCREMENT,
   `login` varchar(200) NOT NULL,
   `name` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
   `titles in diary` varchar(500) NOT NULL,
-  `schedule` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `schedule` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Дамп данных таблицы `clients`
+--
+
 
 -- --------------------------------------------------------
 
@@ -74,62 +90,19 @@ CREATE TABLE `clients` (
 -- Структура таблицы `kids`
 --
 
-CREATE TABLE `kids` (
-  `id` int(250) NOT NULL,
+CREATE TABLE IF NOT EXISTS `kids` (
+  `id` int(250) NOT NULL AUTO_INCREMENT,
   `title` varchar(500) CHARACTER SET utf8 NOT NULL,
   `master` varchar(500) CHARACTER SET utf8 NOT NULL,
   `schedule` varchar(500) CHARACTER SET utf8 NOT NULL,
   `price` bigint(255) NOT NULL,
   `sold` bigint(255) NOT NULL DEFAULT '0',
   `age` int(100) NOT NULL,
-  `tour` varchar(200) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `tour` varchar(200) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
 --
--- Индексы сохранённых таблиц
+-- Дамп данных таблицы `kids`
 --
 
---
--- Индексы таблицы `adult`
---
-ALTER TABLE `adult`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `clients`
---
-ALTER TABLE `clients`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `kids`
---
-ALTER TABLE `kids`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `adult`
---
-ALTER TABLE `adult`
-  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `clients`
---
-ALTER TABLE `clients`
-  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `kids`
---
-ALTER TABLE `kids`
-  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
