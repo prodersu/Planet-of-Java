74b496436384c1ab5a76ada9d005a4d096a196ea
DrDersu <58595037+DrDersu@users.noreply.github.com>
2019-12-27 11:13:44 +0600

    Add files via upload

--- dance_school.sql
+++ dance_school.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Дек 11 2019 г., 07:00
--- Версия сервера: 10.1.13-MariaDB
--- Версия PHP: 5.6.23
+-- Время создания: Дек 27 2019 г., 06:09
+-- Версия сервера: 10.4.10-MariaDB
+-- Версия PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -31,7 +33,7 @@ CREATE TABLE `admin` (
   `login` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
   `name` varchar(500) NOT NULL,
-  `income` int(250) NOT NULL DEFAULT '0'
+  `income` int(250) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -53,7 +55,7 @@ CREATE TABLE `adult` (
   `master` varchar(500) CHARACTER SET utf8 NOT NULL,
   `schedule` varchar(500) CHARACTER SET utf8 NOT NULL,
   `price` bigint(255) NOT NULL,
-  `sold` bigint(255) DEFAULT '0',
+  `sold` bigint(255) DEFAULT 0,
   `style` varchar(500) CHARACTER SET utf8 NOT NULL,
   `gender` varchar(500) CHARACTER SET utf8 NOT NULL,
   `indiv_group` varchar(500) CHARACTER SET utf8 NOT NULL
@@ -64,7 +66,10 @@ CREATE TABLE `adult` (
 --
 
 INSERT INTO `adult` (`id`, `title`, `master`, `schedule`, `price`, `sold`, `style`, `gender`, `indiv_group`) VALUES
-(1, 'Valse', 'Trevor', 'Tuesday', 15000, 0, 'Ballroom dance', 'Woman', 'Group');
+(1, 'Valse', 'Trevor', 'Tuesday', 15000, 0, 'Ballroom dance', 'Woman', 'Group'),
+(2, 'Salsa', 'Antonio', 'Monday Friday ', 30000, 0, 'Club Latine', 'Woman', 'Individual'),
+(3, 'Yoga', 'Shifu Fo', 'Wednesday Friday Saturday', 0, 1, 'Fitness Yoga', 'all', 'Group'),
+(4, 'Fitness', 'Shifu Fo', 'Tuesday Thursday ', 0, 0, 'Fitness Yoga', 'all', 'Group');
 
 -- --------------------------------------------------------
 
@@ -86,7 +91,7 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`id`, `login`, `name`, `password`, `titles`, `schedule`) VALUES
-(1, 'client', 'Alihan', 'client', '', ''),
+(1, 'client', 'Alihan', 'client', 'Yoga\n', 'Wednesday Friday Saturday\n'),
 (2, 'fresh', 'Alina', 'fresh', '', '');
 
 -- --------------------------------------------------------
@@ -101,7 +106,7 @@ CREATE TABLE `kids` (
   `master` varchar(500) CHARACTER SET utf8 NOT NULL,
   `schedule` varchar(500) CHARACTER SET utf8 NOT NULL,
   `price` bigint(255) NOT NULL,
-  `sold` bigint(255) NOT NULL DEFAULT '0',
+  `sold` bigint(255) NOT NULL DEFAULT 0,
   `age` int(100) NOT NULL,
   `tour` varchar(200) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -111,7 +116,10 @@ CREATE TABLE `kids` (
 --
 
 INSERT INTO `kids` (`id`, `title`, `master`, `schedule`, `price`, `sold`, `age`, `tour`) VALUES
-(1, 'Balet', 'Lebedeva', 'Monday', 15000, 0, 10, 'no');
+(1, 'Balet', 'Lebedeva', 'Monday', 15000, 0, 10, 'no'),
+(2, 'Folk', 'Dariga', 'Tuesday Saturday ', 17000, 0, 15, 'yes'),
+(3, 'Break dance', 'Johny', 'Tuesday Friday ', 3000, 0, 12, 'no'),
+(4, 'Fortnite', 'Screamer', 'Friday  ', 4500, 0, 7, 'no');
 
 --
 -- Индексы сохранённых таблиц
@@ -150,21 +158,26 @@ ALTER TABLE `kids`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT для таблицы `adult`
 --
 ALTER TABLE `adult`
-  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT для таблицы `clients`
 --
 ALTER TABLE `clients`
   MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT для таблицы `kids`
 --
 ALTER TABLE `kids`
-  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
