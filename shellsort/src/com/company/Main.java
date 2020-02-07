4421b4275c6d280b4ca23326fb5e79ea4f33225b
DrDersu <58595037+DrDersu@users.noreply.github.com>
2020-02-07 14:03:17 +0600

    Update Main.java

--- shellsort/src/com/company/Main.java
+++ shellsort/src/com/company/Main.java
@@ -1,68 +1,290 @@
-package shellsort.src.com.company;
+package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Formatter;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private int len;
-    private Merge merge = null;
-    static void to_txt(int[] arr, int len) {
-        try {
-            
-            Formatter f = new Formatter(Integer.toString(len)+".txt");
-            for (int i = 0; i < len; i++) {
-                f.format("%s", Integer.toString(arr[i]) + " ");
-                if (i % 100 == 0 && i > 0) f.format("%s", "\r\n");
-            }
-            f.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public Main() throws FileNotFoundException {        
-        System.out.println("Input the size: ");
-        int[] arr = new int[360005];
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        len = n;
-        Random r = new Random();
-        for (int i = 0; i < n; i++) arr[i] = r.nextInt(1000);
-        to_txt(arr, n);
-        System.out.println(arr[0] + "-------" + arr[n - 1]);
-        System.out.println("The text file with numbers of array was created successfully");
-        System.out.println("Choose the sort: ");
-        System.out.println("1. Bubble");
-        System.out.println("2. Merge");
-        System.out.println("3. Heap");
-        System.out.println("4. Radix");
-        System.out.println("5. Shell");
-        System.out.println("6. Insertion");
-        System.out.println("7. Cocktail \n");
-        int ch = sc.nextInt();
-        while(true){
-            if(ch==1){
-                getMerge();
-            }
-            else {
-                System.out.println("Do the right choise, please\n");
+
+    public static void main(String args[]) throws FileNotFoundException {
+
+        System.out.println("Array size  | Bubble|   Merge |   Quick |   Heap  |  Radix ");
+        int [][] table = new int[6][6];
+        int k = 1;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if(j==0){
+                    table[i][j] = k*60000;
+                    k++;
+                }
             }
         }
+        //bubble sorting
+        for(int i = 0; i<6; i++){
+            int len = table[i][0];
+            int [] arr_b = new int[len];
+            File f = new File(Integer.toString(len)+".txt");
+            Scanner sc2 = new Scanner(f);
+            int j = 0;
+            while(sc2.hasNext()){
+                arr_b[j++] = Integer.parseInt(sc2.next());
+            }
+            sc2.close();
+            long start = System.currentTimeMillis();
+            b_sort(arr_b, len);
+            long end = System.currentTimeMillis();
+            int time = Integer.parseInt(Long.toString(end - start));
+            table[i][1] = time;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print(table[i][j] + "         ");
+            }
+            System.out.println();
+        }
+
+
     }
-    public void printTime(String sort, long time, float time1){
-        System.out.println(sort + " sort performed this task for: ");
-        System.out.println("time: " + time +  " seconds " + time1 + " milliseconds\n");
+
+//radix sort
+static int getMax(int arr[], int n)
+    {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
     }
-    public int getLen(){
-        return len;
+
+    // A function to do counting sort of arr[] according to
+    // the digit represented by exp.
+    static void countSort(int arr[], int n, int exp)
+    {
+        int output[] = new int[n]; // output array
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count, 0);
+
+        // Store count of occurrences in count[]
+        for (i = 0; i < n; i++)
+            count[ (arr[i]/exp)%10 ]++;
+
+        // Change count[i] so that count[i] now contains
+        // actual position of this digit in output[]
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        // Build the output array
+        for (i = n - 1; i >= 0; i--)
+        {
+            output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+            count[ (arr[i]/exp)%10 ]--;
+        }
+
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to curent digit
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
     }
-    public Merge getMerge(){
-        return merge;
+
+    // The main function to that sorts arr[] of size n using
+    // Radix Sort
+    static void radixsort(int arr[], int n) {
+        // Find the maximum number to know number of digits
+        int m = getMax(arr, n);
+
+        // Do counting sort for every digit. Note that instead
+        // of passing digit number, exp is passed. exp is 10^i
+        // where i is current digit number
+        for (int exp = 1; m / exp > 0; exp *= 10)
+            countSort(arr, n, exp);
     }
+//Bubble sort
+    static void b_sort(int[] arr, int len) {
+        boolean swap = true;
+        while (swap) {
+            swap = false;
+            for (int i = 0; i < len - i - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swap = true;
+                }
+            }
+        }
+    }
+    //Merge sort
+
+    void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void m_sort(int arr[], int l, int r)
+    {
+        if (l < r)
+        {
+            // Find the middle point
+            int m = (l+r)/2;
+
+            // Sort first and second halves
+            m_sort(arr, l, m);
+            m_sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    //heap sort
+
+    static void h_sort(int arr[], int n)
+    {
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int i=n-1; i>=0; i--)
+        {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    static void heapify(int arr[], int n, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2*i + 1; // left = 2*i + 1
+        int r = 2*i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i)
+        {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    //quick sort
+    static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than the pivot
+            if (arr[j] < pivot) {
+                i++;
+
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+
+    /* The main function that implements QuickSort()
+      arr[] --> Array to be sorted,
+      low  --> Starting index,
+      high  --> Ending index */
+    static void q_sort(int arr[], int low, int high) {
+        if (low < high) {
+            /* pi is partitioning index, arr[pi] is
+              now at right place */
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before
+            // partition and after partition
+            q_sort(arr, low, pi - 1);
+            q_sort(arr, pi + 1, high);
+        }
+    }
+
+
 }
-
-
-
